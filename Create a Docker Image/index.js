0381ac19692971a7591bd5e319969e00d475af4e
ITZ-HURAIRAH18 <muhammadabuhurairah558@gmail.com>
2026-01-24 07:04:02 +0500

    Create Docker Image

---
+++ Create a Docker Image/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+// Route
+app.get('/', (req, res) => {
+  res.send('Express server is running 🚀');
+});
+
+// Server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
